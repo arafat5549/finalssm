3c95a21ad340a28a63108578d6781ec14398d5be
arafat5549 <arafat5549@gmail.com>
2017-05-16 17:54:34 +0800

    Mybatsi自动生成语句完成版

--- src/test/java/com/ssf/dao/UserDaoTest.java
+++ src/test/java/com/ssf/dao/UserDaoTest.java
@@ -1,6 +1,8 @@
 package com.ssf.dao;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -8,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import com.ssf.common.vo.mybatis.pagination.Page;
 import com.ssf.model.User;
 
 
@@ -24,11 +27,6 @@ public class UserDaoTest {
     @Autowired
     private UserDao userDao;
     
-//    @BeforeClass
-//    public static void pre(){
-//    	System.out.println("----------------------");
-//    }
-    
     
 	
 	@Test
@@ -38,14 +36,20 @@ public class UserDaoTest {
 		System.out.println("--1------------------------");
 	}
 
-//	@Test
-//	public void testListPage() {
-//		List<User> list=userDao.listPage(0, 100);
-//		for (User user : list) {
-//			System.out.println(user);
-//		}
-//		System.out.println("--2------------------------");
-//	}
+	@Test
+	public void testListPage() {
+		Map<Object , Object> map = new HashMap<Object, Object>();
+		//Page page = new Page(0, 10);
+		//map.put("page", page);
+		//map.put("databaseId","mysql");
+		map.put("name", "wang");
+		
+		List<User> list=userDao.selectListByMap(map);
+		for (User user : list) {
+			System.out.println(user);
+		}
+		System.out.println("--2------------------------");
+	}
 //	
 //	//@Test
 //	public void testAddScore() {
